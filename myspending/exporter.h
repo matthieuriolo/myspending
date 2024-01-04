c0e990e18cdb05407e6f2ce995757b3a9b2e306c
Matthieu Riolo <matthieu.riolo@qxmail.ch>
2024-01-04 01:24:40 +0100

    cleanup exporter

--- myspending/exporter.h
+++ myspending/exporter.h
@@ -18,15 +18,16 @@ public:
     void beforeTraversingEntries(int categoryId);
     void traverseEntry(QString description, int type, double value, int categoryId);
     void afterTraversingEntries(int categoryId);
-private:
 
-    QDataStream *dataStream = nullptr;
 
     constexpr static auto HEADER_MIMETYPE = "application/myspending";
     constexpr static auto HEADER_VERSION = "1";
     constexpr static auto HEADER_SEPARATOR = "\n";
     constexpr static qint8 DATA_NEXT_OBJECT_EXISTS = 0x0;
     constexpr static qint8 DATA_NO_OTHER_OBJECT_EXISTS = 0x1;
+private:
+
+    QDataStream *dataStream = nullptr;
 };
 
 #endif // EXPORTER_H
